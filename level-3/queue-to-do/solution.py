48c5c59f87c93459b747d690c963431c5ea3783c
CrunchyDuck <43281440+CrunchyDuck@users.noreply.github.com>
2021-02-07 06:58:29 +0000

    Implement fast XOR
    I believed my code was too slow for verification.
    Researching showed a way to calculate the XOR of a range quickly.
    Code based off of https://stackoverflow.com/questions/10670379/find-xor-of-all-numbers-in-a-given-range

--- level-3/queue-to-do/solution.py
+++ level-3/queue-to-do/solution.py
@@ -1,3 +1,22 @@
+def f(a):
+    res = [a, 1, a + 1, 0]
+    return res[a % 4]
+
+def get_xor(start, end):
+    """
+    Gets the XOR of a range of numbers from start to end.
+    Solution inspired by:
+    https://stackoverflow.com/questions/10670379/find-xor-of-all-numbers-in-a-given-range
+
+    Arguments:
+        start - The first number in the range
+        end - The last number in the range.
+
+    Returns:
+        The XOR of all of the numbers in the range.
+    """
+    return f(end) ^ f(start - 1)
+
 def solution(start, length):
     """
     Calculates the security checksum for a checkpoint with the given values.
@@ -8,14 +27,17 @@ def solution(start, length):
     Returns:
         (int) Checksum for this checkpoint.
     """
-    row = 0
     cap = length
     checksum = 0
 
     while cap > 0:
-        row_start = start + (length * row)  # First number in row.
-        for i in range(cap):
-            checksum ^= (row_start + i)
-        row += 1
+        checksum ^= get_xor(start, start + cap - 1)
+        start = start + length
         cap -= 1
+
     return checksum
+
+print solution(17, 4)
+#print solution(5, 500)
+#print solution(200000000, 2)
+print solution(2, 2000000)
