08b6b33ced4db36bfcc25c0f7c92dc4642b4b561
CrunchyDuck <43281440+CrunchyDuck@users.noreply.github.com>
2021-02-20 08:13:40 +0000

    Calculate bunny_keys

--- level-4/free-the-bunny-workers/solution.py
+++ level-4/free-the-bunny-workers/solution.py
@@ -10,11 +10,7 @@ def test_union(num_required, bunny_keys, union):
         num_required: Number of bunnies to pick from the pile
         bunny_keys: A list of all the keys each bunny has
         union: What the union of the bunny keys should be.
-
-    Returns:
-
     """
-    this_set = set()
     union = set(union)
 
     # Create all different combinations of bunnies that can be picked,
@@ -54,7 +50,27 @@ def num_doors(num_bunnies, num_required):
         Number of doors.
     """
     binomial = num_required - 1
-    n = num_bunnies# + binomial
+    n = num_bunnies
+    return binomial_coeff(n, binomial)
+
+
+def num_bunny_keys(num_bunnies, num_required):
+    """
+    Calculates the number of doors there must be, given num_bunnies and num_required.
+    Essentially a binomial coefficient calculator with offsets.
+    Arguments:
+        num_bunnies: Number of bunnies
+        num_required: Number of bunnies required to open an amount of doors.
+
+    Returns:
+        Number of doors.
+    """
+    binomial = num_required - 1
+    n = num_bunnies - 1
+    return binomial_coeff(n, binomial)
+
+
+def binomial_coeff(n, binomial):
     return fac(n) // fac(binomial) // fac(n - binomial)
 
 
@@ -65,27 +81,23 @@ def flatten_gen(generator):
 def solution(num_bunnies, num_required):
     # short circuits
     if num_required == 0:  # I don't actually know what I should be returning for this one.
-        return [[] for _ in range(num_required)]
+        return [[] for _ in range(num_bunnies)]
     elif num_required == 1:
-        return [[0] for _ in range(num_required)]
+        return [[0] for _ in range(num_bunnies)]
     elif num_required == num_bunnies:
-        return [[x] for x in range(num_required)]
+        return [[x] for x in range(num_bunnies)]
 
     door_num = num_doors(num_bunnies, num_required)
     doors = [x for x in range(door_num)]
     bunny_keys = 1  # How many keys each bunny has.
 
-    while bunny_keys <= door_num:  # If this breaks due to condition, something has gone wrong.
-        combins = flatten_gen(combinations(doors, bunny_keys))
-        if len(combins) < num_bunnies:
-            break
-        for bunny_set in combinations(combins, num_bunnies):
+    combins = flatten_gen(combinations(doors, bunny_keys))
+    for bunny_set in combinations(combins, num_bunnies):
             result = test_union(num_required, bunny_set, doors)
             if result[0]:
                 return result
             else:
                 pass #print result
-        bunny_keys += 1
 
     raise Exception("Could not find a combination of bunny keys.\n"
                     "num bunnies:%s\n"
@@ -95,13 +107,13 @@ def solution(num_bunnies, num_required):
 
 
 def tst():
-    print solution(9, 2)
+    print solution(4, 3)
 
 
 #print num_doors(5, 3)
 
-#cProfile.run("tst()", sort="cumtime")
+cProfile.run("tst()", sort="cumtime")
 #test_union(2, ((0, 1, 2), (0, 1, 3), (0, 2, 3), (1, 2, 3)))
-#print solution(2, 1)
+#print solution(4, 3)
 #print solution(4, 4)
-print solution(5, 3)
+#print solution(5, 3)
